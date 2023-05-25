26b11b674b2f52a812266ba54da520d47a82f01e
Olshin Anton <olshin.ant@yandex.ru>
2023-05-25 23:29:21 +0300

    added some flake8 edits

--- src/training.py
+++ src/training.py
@@ -2,6 +2,7 @@
 Script for training emotion classification model with
 PCA + LDA to reduce dimentions and SVM model for classification.
 """
+
 import cv2
 import pickle
 import numpy as np
@@ -74,10 +75,10 @@ def lda_classification(X_featured, df_target, write_folder):
     # drop_indices = np.random.choice(downsample.index, int(len(downsample)/2), replace=False)
     # resampled_df = resampled_df.drop(drop_indices)
 
-    resampled_df = resampled_df.reset_index(drop=True)    
+    resampled_df = resampled_df.reset_index(drop=True)
 
     y_train = resampled_df.label
-    X_train = resampled_df.drop('label', axis=1)
+    X_train = resampled_df.drop("label", axis=1)
 
     # PCA + LDA to dimensionality reduction
     scaler = StandardScaler()
@@ -103,46 +104,43 @@ def lda_classification(X_featured, df_target, write_folder):
 
     # SVM classification
     clf = SVC()
-    params = {'C': [1, 10, 100, 1000], 'gamma': [1, 0.1, 0.001, 0.0001], 'degree': range(1, 15), 'kernel': ['linear', 'rbf']}
-    search = GridSearchCV(clf, params, scoring="f1_weighted", cv=5, n_jobs=-1)
+    par = {'C': [1, 10, 100, 1000], "gamma": [1, 0.1, 0.001, 0.0001],
+              "degree": range(1, 15),"kernel": ["linear", "rbf"]}
+    search = GridSearchCV(clf, par, scoring="f1_weighted", cv=5, n_jobs=-1)
     search.fit(x_train_lda, y_train)
 
     SVC_model = search.best_estimator_
     SVC_model.fit(x_train_lda, y_train)
-    SVC_prediction = SVC_model.predict(x_test_lda) 
+    SVC_prediction = SVC_model.predict(x_test_lda)
 
     report = classification_report(y_test, SVC_prediction, output_dict=True)
     conf_matrix = confusion_matrix(SVC_prediction, y_test)
     df_conf_matrix = pd.DataFrame(conf_matrix)
     df_report = pd.DataFrame(report).transpose()
 
+    with open(write_folder.joinpath("scaler.pkl"), "wb") as file:
+        pickle.dump(scaler, file)
 
-    with open(write_folder.joinpath('scaler.pkl'), 'wb') as f:
-        pickle.dump(scaler, f)
 
-    with open(write_folder.joinpath('pca.pkl'), 'wb') as f:
-        pickle.dump(pca, f)
+    with open(write_folder.joinpath("pca.pkl"), "wb") as file:
+        pickle.dump(pca, file)
 
-    with open(write_folder.joinpath('pca.pkl'), 'wb') as f:
-        pickle.dump(pca, f)
+    with open(write_folder.joinpath("lda_model.pkl"), "wb") as file:
+        pickle.dump(LDA_model, file)
 
-    with open(write_folder.joinpath('lda_model.pkl'), 'wb') as f:
-        pickle.dump(LDA_model, f)
-
-    with open(write_folder.joinpath('svd_model.pkl'), 'wb') as f:
-        pickle.dump(SVC_model, f)
+    with open(write_folder.joinpath("svd_model.pkl"), "wb") as file:
+        pickle.dump(SVC_model, file)
 
     df_report.to_excel(write_folder.joinpath('classification_report.xlsx'))
-    df_conf_matrix.to_excel(write_folder.joinpath('confusion_matrix.xlsx'))
+    df_conf_matrix.to_excel(write_folder.joinpath("confusion_matrix.xlsx"))
 
 
 if __name__ == "__main__":
 
     project_folder = Path(__file__).parents[1].resolve()
-    image_folder = project_folder.joinpath('data/processed/images')
-    target_path = project_folder.joinpath('data/processed/df_target.csv')
-    write_folder = project_folder.joinpath('models')
-
+    image_folder = project_folder.joinpath("data/processed/images")
+    target_path = project_folder.joinpath("data/processed/df_target.csv")
+    write_folder = project_folder.joinpath("models")
 
     img_files_list = get_list_of_files(image_folder)
     df_target = pd.read_csv(target_path)
